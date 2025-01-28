5936c2d362bb8ef342745298bca1ec54eea778f6
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-01-28 19:14:57 +0530

    Feat: completed basic required operations an llm with the context of our repo

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -15,28 +15,55 @@ class ChatService:
     """
 
     def __init__(self):
-        self.openaikey = os.getenv("OPEN_API_KEY")
+        self.llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=os.getenv("OPEN_API_KEY"))
 
-    def get_session_history(self, session_id: str) -> InMemoryChatMessageHistory:
+    def get_session_history(
+        self,session_id: str, config: dict = None
+    ) -> InMemoryChatMessageHistory:
         """
-        helper function to access the session
+        Retrieves or creates a chat message history for a given session ID.
+
+        Args:
+            session_id (str): A unique identifier for the session.
+
+        Returns:
+            InMemoryChatMessageHistory: The chat message history associated with the session ID.
+                                    If the session ID does not exist, a new history is created
+                                    and initialized with a system message.
         """
         if session_id not in store:
             store[session_id] = InMemoryChatMessageHistory()
-            # remember to add all context here
             store[session_id].add_message(
                 SystemMessage(
-                    content="My name is muhammed sirajudeen add my name in all responses"
+                    content=config.get("context")
                 )
             )
         return store[session_id]
 
-    def chat(self, query: str, session_id: str) -> str:
+
+    def chat(self, query: str, session_id: str,context:str) -> str:
         """
         used to chat
         """
-        return self.openaikey
+        chain = RunnableWithMessageHistory(
+            self.llm,
+            lambda session_id: self.get_session_history(
+                session_id, config={"context": context}
+            ),
+        )
+        response = chain.invoke(
+            query, config={"configurable": {"session_id": session_id, "context": context}}
+        )
+        print(response.content)
+        return response.content
 
+    def clear_chat(self,session_id:str)->str:
+        '''
+        used to clear the current memory of user
+        '''
+        if session_id in store:
+            del store[session_id]
+        return "chat cleared"
 
 def get_chat_service() -> ChatService:
     """
